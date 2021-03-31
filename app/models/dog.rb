f76d530523b127b4351a974d97045bac143aa214
Hatice <haticekaraty@gmail.com>
2021-03-31 13:52:23 -0400

    commit tree 96a48338c72f965454f9677fc92d4ca73422f081

---
+++ app/models/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    @@all = []
+    attr_accessor :name, :breed, :age
+    def initialize(name, breed, age)
+        self.name = name
+        self.breed = breed
+        self.age = age
+        self.save
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
